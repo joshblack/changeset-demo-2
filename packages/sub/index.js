17ff56de5b6dccb8f048ada0b8210a07148d1312
Josh Black <josh@josh.black>
2021-06-29 19:04:56 -0500

    feat(sub): add variadic support (#8)
    * feat(sub): add variadic support
    
    * chore(project): add changeset

--- packages/sub/index.js
+++ packages/sub/index.js
@@ -5,6 +5,12 @@
  * @param {number} y
  * @returns {number}
  */
-module.exports = function sub(x, y) {
-  return x - y;
+module.exports = function sub(...numbers) {
+  let result = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    result -= numbers[i];
+  }
+
+  return result;
 };
